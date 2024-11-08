f9d7ab703d0d49afd1e5eb8e6c9bb1e5c8c0ef1a
Axel Alfredo Sanchez Ortega <proaxel1214@gmail.com>
2024-11-07 20:46:41 -0600

    commit final servicios terminados

--- src/app.py
+++ src/app.py
@@ -77,6 +77,44 @@ def leer_alumnos(mat):
 
 
 """ ----------------------------------------------------------------------------------------------------------------------------------- """
+@app.route('/alumnos/<mat>', methods=['PUT'])
+def actualizar_curso(mat):
+    #if (validar_matricula(mat) and validar_nombre(request.json['nombre']) and validar_apaterno(request.json['apaterno'])):
+        try:
+            alumno = leer_alumno_bd(mat)
+            if alumno != None:
+                cursor = con.connection.cursor()
+                sql = """UPDATE alumnos SET nombre = '{0}', apaterno = '{1}', amaterno='{2}', correo='{3}'
+                WHERE matricula = {4}""".format(request.json['nombre'], request.json['apaterno'], request.json['amaterno'],request.json['correo'], mat)
+                cursor.execute(sql)
+                con.connection.commit()  # Confirma la acción de actualización.
+                return jsonify({'mensaje': "Alumno actualizado.", 'exito': True})
+            else:
+                return jsonify({'mensaje': "Alumno no encontrado.", 'exito': False})
+        except Exception as ex:
+            return jsonify({'mensaje': "Error {0} ".format(ex), 'exito': False})
+    #else:
+     #   return jsonify({'mensaje': "Parámetros inválidos...", 'exito': False})
+ 
+ 
+@app.route('/alumnos/<mat>', methods=['DELETE'])
+def eliminar_curso(mat):
+    try:
+        alumno = leer_alumno_bd(mat)
+        if alumno != None:
+            cursor = con.connection.cursor()
+            sql = "DELETE FROM alumnos WHERE matricula = {0}".format(mat)
+            cursor.execute(sql)
+            con.connection.commit()  # Confirma la acción de eliminación.
+            return jsonify({'mensaje': "Alumno eliminado.", 'exito': True})
+        else:
+            return jsonify({'mensaje': "Alumno no encontrado.", 'exito': False})
+    except Exception as ex:
+        return jsonify({'mensaje': "Error", 'exito': False})
+
+
+
+
 
 def pagina_no_encontrada(error):
     return"<h1>Pagina no encontrada</h1>"    
@@ -84,5 +122,7 @@ if __name__=="__main__":
     app.config.from_object(config['development'])
     app.register_error_handler(404,pagina_no_encontrada)
     app.run(host='0.0.0.0',port=5000)
+
+
 
 
